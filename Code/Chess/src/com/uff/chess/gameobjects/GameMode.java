57efeb84486da927d47e9ab14cfe69ad3f0ef652
GF-Ribeiro <gabriel_ribeiro@id.uff.br>
2017-06-01 17:33:36 -0300

    Menu final
    Menu com créditos, modos de jogo e dificuldades

---
+++ Code/Chess/src/com/uff/chess/gameobjects/GameMode.java
@@ -0,0 +1,32 @@
+/*
+*
+* Chess Game
+* Software Engineering II - Universidade Federal Fluminense
+*
+ */
+package com.uff.chess.gameobjects;
+
+import com.vpontes.gameframework.math.Vector2;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author gabri
+ */
+public class GameMode extends GameObject
+{
+    
+    public GameMode(Vector2 position, int widght, int height,  BufferedImage image) {
+        super(position, widght, height, image);
+        
+    }
+    
+   
+    
+    @Override
+    public void draw(Graphics g) 
+    {
+        super.draw(g);
+    }
+}
